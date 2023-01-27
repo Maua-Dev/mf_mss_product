08a057d9dcc6f41a1983cee184cd2d6b843e279b
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2023-01-27 12:41:53 -0300

    create entity product with test

---
+++ src/shared/domain/enums/meal_type_enum.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class MEAL_TYPE(Enum):
+    SNACKS = "SNACKS"
+    DRINKS = "DRINKS"
+    CANDY = "CANDY"
+    PLATES = "PLATES"
+    PORTIONS = "PORTIONS"
+    TIDBIT = "TIDBIT"
+    PASTAS = "PASTAS"
+    SALAD = "SALAD"
+    
---
+++ src/shared/domain/enums/restaurant_enum.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class RESTAURANT(Enum):
+    SOUZA_DE_ABREU = "Souza de Abreu"
+    RESTAURANTE_DO_H = "Restaurante do H "
+    
