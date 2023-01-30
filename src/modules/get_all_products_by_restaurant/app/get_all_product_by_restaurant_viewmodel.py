a0296e3bde12c4b3f91308d3b8b278037132f43c
LucasKiller <lucalhardo@gmail.com>
2023-01-30 17:26:05 -0300

    get all products by restaurant viewmodel with TU 100% 🍆

---
+++ src/modules/get_all_products_by_restaurant/app/get_all_product_by_restaurant_viewmodel.py
@@ -0,0 +1,41 @@
+from src.shared.domain.enums.restaurant_enum import RESTAURANT
+from src.shared.domain.entities.product import Product
+from typing import List
+
+class ProductViewmodel:
+    product: Product
+
+    def __init__(self, product: Product):
+        self.product = product
+
+    def to_dict(self) -> dict:
+        return{
+            "available": self.product.available,
+             "price": self.product.price,
+             "name": self.product.name,
+             "description": self.product.description,
+             "meal_type": self.product.meal_type.value,
+             "photo": self.product.photo,
+             "product_id": self.product.product_id,
+             "last_update": self.product.last_update,
+             "restaurant": self.product.restaurant.value,
+             "prepareTime": self.product.prepareTime 
+            }
+
+
+class GetAllProductsByRestaurantViewmodel:
+    all_products: List[ProductViewmodel]
+    
+    def __init__(self, all_products: List[Product]):
+        self.products = [product for product in all_products]
+
+    def to_dict(self) -> dict:
+        return{
+            "Souza de Abreu": [ProductViewmodel(product).to_dict() for product in self.products if product.restaurant == RESTAURANT.SOUZA_DE_ABREU],
+            "Restaurante do H": [ProductViewmodel(product).to_dict() for product in self.products if product.restaurant == RESTAURANT.RESTAURANTE_DO_H],
+            "message": "the products were retrived"
+        }
+
+    
+        
+    
