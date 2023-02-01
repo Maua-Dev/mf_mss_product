935e0fb6791246e94ccc6d2204b446554740e3e9
RodrigoM2004 <rodrigo.monasterios@outlook.com>
2023-02-01 12:44:03 -0300

    FOI O MILAS

--- tests/modules/get_all_products_by_restaurant/app/test_get_all_products_by_restaurant_usecase.py
+++ tests/modules/get_all_products_by_restaurant/app/test_get_all_products_by_restaurant_usecase.py
@@ -6,18 +6,18 @@ from src.shared.infra.repositories.product_repository_mock import ProductReposit
 
 
 class Test_GetAllProductByRestaurantUsecase:
-    def test_get_all_product_by_restarant_usecase_rest1(self):
+    def test_get_all_product_by_restaurant_usecase_souza_de_abreu(self):
         repo = ProductRepositoryMock()
         usecase = GetAllProductsByRestaurantUsecase(repo=repo)
 
-        product_rest1 = usecase(restaurant=RESTAURANT.SOUZA_DE_ABREU)
+        product_souza_de_abreu = usecase(restaurant=RESTAURANT.SOUZA_DE_ABREU)
 
-        assert all([product.restaurant == RESTAURANT.SOUZA_DE_ABREU for product in product_rest1])
+        assert all([product.restaurant == RESTAURANT.SOUZA_DE_ABREU for product in product_souza_de_abreu])
 
-    def test_get_all_product_by_restarant_usecase_rest2(self):
+    def test_get_all_product_by_restaurant_usecase_restaurante_do_h(self):
         repo = ProductRepositoryMock()
         usecase = GetAllProductsByRestaurantUsecase(repo=repo)
 
-        product_rest2 = usecase(restaurant=RESTAURANT.RESTAURANTE_DO_H)
+        product_restaurante_do_h = usecase(restaurant=RESTAURANT.RESTAURANTE_DO_H)
 
-        assert all([product.restaurant == RESTAURANT.RESTAURANTE_DO_H for product in product_rest2])
+        assert all([product.restaurant == RESTAURANT.RESTAURANTE_DO_H for product in product_restaurante_do_h])
