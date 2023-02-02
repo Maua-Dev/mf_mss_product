c7f6123aad3a8e1200082cafc3af150152a48df3
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2023-02-01 22:54:36 -0300

    get_all_products_group_by_restaurant UC

---
+++ tests/modules/get_all_products_group_by_restaurant/app/test_get_all_products_group_by_restaurant_usecase.py
@@ -0,0 +1,18 @@
+from src.modules.get_all_products_group_by_restaurant.app.get_all_products_group_by_restaurant_usecase import GetAllProductsGroupByRestaurantUsecase
+from src.shared.domain.enums.restaurant_enum import RESTAURANT
+from src.shared.infra.repositories.product_repository_mock import ProductRepositoryMock
+
+
+class Test_GetAllProductsGroupByRestaurantUsecase:
+    def test_get_all_products_group_by_restaurant_usecase(self):
+        repo = ProductRepositoryMock()
+        usecase = GetAllProductsGroupByRestaurantUsecase(repo=repo)
+        products = usecase()
+        
+        assert list(products.keys()) == [restaurant for restaurant in RESTAURANT]
+        
+        lenght_products = 0
+        for list_of_products in products.values():
+            for product in list_of_products:
+                lenght_products += 1      
+        assert lenght_products == len(repo.products)
