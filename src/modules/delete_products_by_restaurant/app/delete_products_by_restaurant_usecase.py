1cde5eb3ab1d897a7b441a1cdb9aab852cb8fe4b
LucasKiller <lucalhardo@gmail.com>
2023-02-06 19:59:39 -0300

    delete product by restaurant usecase and TU 100%

---
+++ src/modules/delete_products_by_restaurant/app/delete_products_by_restaurant_usecase.py
@@ -0,0 +1,26 @@
+from typing import Dict, List
+from src.shared.domain.entities.product import Product
+from src.shared.domain.enums.restaurant_enum import RESTAURANT
+from src.shared.domain.repositories.product_repository_interface import IProductRepository
+from src.shared.helpers.errors.domain_errors import EntityError
+from src.shared.helpers.errors.usecase_errors import NoItemsFound
+
+
+class DeleteProductsByRestaurantUsecase:
+    def __init__(self, repo: IProductRepository):
+        self.repo = repo
+
+    def __call__(self, product_id: int, restaurant: RESTAURANT) -> Dict[RESTAURANT, List[Product]]:
+        if type(product_id) != int:
+            raise EntityError("product_id")
+
+        if type(restaurant) != RESTAURANT:
+            raise EntityError("restaurant")
+
+        if product_id is None:
+            raise NoItemsFound("product_id")
+
+        if restaurant is None:
+            raise NoItemsFound("restaurant")
+        
+        return self.repo.delete_products_by_restaurant(product_id=product_id, restaurant=restaurant)
---
+++ tests/modules/delete_products_by_restaurant/app/test_delete_products_by_restaurant_usecase.py
@@ -0,0 +1,46 @@
+import pytest
+from src.modules.delete_products_by_restaurant.app.delete_products_by_restaurant_usecase import DeleteProductsByRestaurantUsecase
+from src.shared.domain.enums.restaurant_enum import RESTAURANT
+from src.shared.infra.repositories.product_repository_mock import ProductRepositoryMock
+from src.shared.helpers.errors.usecase_errors import NoItemsFound
+from src.shared.helpers.errors.domain_errors import EntityError
+
+
+class Test_DeleteProductsByRestaurantUsecase:
+    def test_delete_products_by_restaurant_usecase(self):
+        repo = ProductRepositoryMock()
+        usecase = DeleteProductsByRestaurantUsecase(repo=repo)
+        lenBefore = len(repo.products)
+        product = usecase(product_id=0, restaurant=RESTAURANT.SOUZA_DE_ABREU)
+
+        assert len(repo.products) == lenBefore - 1
+        assert [product.product_id, product.restaurant] == [0, repo.products[0].restaurant]
+
+    def test_delete_products_by_restaurant_diff_type_product_id(self):
+        repo = ProductRepositoryMock()
+        usecase = DeleteProductsByRestaurantUsecase(repo=repo)
+
+        with pytest.raises(EntityError):
+            product = usecase(product_id="0", restaurant=RESTAURANT.SOUZA_DE_ABREU)
+
+    def test_delete_products_by_restaurant_diff_type_restaurant(self):
+        repo = ProductRepositoryMock()
+        usecase = DeleteProductsByRestaurantUsecase(repo=repo)
+
+        with pytest.raises(EntityError):
+            product = usecase(product_id=0, restaurant="SOUZA_DE_ABREU")
+
+    # def test_delete_products_by_restaurant_not_found_product_id(self):
+    #     repo = ProductRepositoryMock()
+    #     usecase = DeleteProductsByRestaurantUsecase(repo=repo)
+
+    #     with pytest.raises(NoItemsFound):
+    #         product = usecase(product_id=-2, restaurant=RESTAURANT.SOUZA_DE_ABREU)
+
+    # def test_delete_products_by_restaurant_not_found_restaurant(self):
+    #     repo = ProductRepositoryMock()
+    #     usecase = DeleteProductsByRestaurantUsecase(repo=repo)
+
+    #     with pytest.raises(NoItemsFound):
+    #         product = usecase(product_id=0, restaurant=RESTAURANT.SOUZA_DE_ABREUD)
+
