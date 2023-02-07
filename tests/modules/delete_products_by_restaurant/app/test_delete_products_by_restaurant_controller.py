7cfc7212bdb24fbfca3885fab73b9daa5c6b226e
LucasKiller <lucalhardo@gmail.com>
2023-02-07 19:44:26 -0300

    different sorts of tests on the controller

--- tests/modules/delete_products_by_restaurant/app/test_delete_products_by_restaurant_controller.py
+++ tests/modules/delete_products_by_restaurant/app/test_delete_products_by_restaurant_controller.py
@@ -24,3 +24,51 @@ class Test_DeleteProductsByRestaurantController:
         assert response.body['product']['restaurant'] == "SOUZA_DE_ABREU"
         assert response.body['message'] == "the product was deleted"
         
+    def test_delete_products_by_restaurant_product_id_is_missing(self):
+        repo = ProductRepositoryMock()
+        usecase = DeleteProductsByRestaurantUsecase(repo=repo)
+        controller = DeleteProductsByRestaurantController(usecase=usecase)
+        
+        request = HttpRequest(
+            body={
+                "restaurant": repo.products[0].restaurant.value
+            }
+        )
+
+        response = controller(request=request)
+
+        assert response.status_code == 400
+        assert response.body == "Field product_id is missing"
+
+    def test_delete_products_by_restaurant_restaurant_is_missing(self):
+        repo = ProductRepositoryMock()
+        usecase = DeleteProductsByRestaurantUsecase(repo=repo)
+        controller = DeleteProductsByRestaurantController(usecase=usecase)
+        
+        request = HttpRequest(
+            body={
+                "product_id": repo.products[0].product_id
+            }
+        )
+
+        response = controller(request=request)
+
+        assert response.status_code == 400
+        assert response.body == "Field restaurant is missing"
+
+    def test_delete_products_by_restaurant_not_found(self):
+        repo = ProductRepositoryMock()
+        usecase = DeleteProductsByRestaurantUsecase(repo=repo)
+        controller = DeleteProductsByRestaurantController(usecase=usecase)
+        
+        request = HttpRequest(
+            body={
+                "product_id": repo.products[0].product_id,
+                "restaurant": "Molas"
+            }
+        )
+
+        response = controller(request=request)
+
+        assert response.status_code == 400
+        assert response.body == "Field restaurant is not valid"
