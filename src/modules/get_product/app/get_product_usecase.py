6143d9ad437ecd107e7fe3a25c5c87c182e0fc1a
LucasKiller <lucalhardo@gmail.com>
2023-04-18 20:46:15 -0300

    get product usecase TU

--- src/modules/get_product/app/get_product_usecase.py
+++ src/modules/get_product/app/get_product_usecase.py
@@ -1,4 +1,5 @@
 from src.shared.domain.entities.product import Product
+from src.shared.domain.enums.restaurant_enum import RESTAURANT
 from src.shared.domain.repositories.product_repository_interface import IProductRepository
 from src.shared.helpers.errors.domain_errors import EntityError
 from src.shared.helpers.errors.usecase_errors import NoItemsFound
@@ -7,7 +8,7 @@ class GetProductUsecase:
     def __init__(self, repo: IProductRepository):
         self.repo = repo
 
-    def __call__(self, product_id: str) -> Product:
+    def __call__(self, product_id: str, restaurant: RESTAURANT) -> Product:
         
         if not Product.validate_product_id(product_id):
             raise EntityError("product_id")
@@ -15,7 +16,10 @@ class GetProductUsecase:
         if type(product_id) is not str:
             raise EntityError("product_id")
         
-        product = self.repo.get_product(product_id)
+        if type(restaurant) is not RESTAURANT:
+            raise EntityError("restaurant")
+        
+        product = self.repo.get_product(product_id=product_id, restaurant=restaurant)
 
         if product is None:
             raise NoItemsFound("product")
