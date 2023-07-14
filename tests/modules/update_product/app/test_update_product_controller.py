64950733c4e90deea147f0e0c6b2f4627d5f0800
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-07-14 17:35:02 -0300

    Add autentication on update controller and UTs

--- tests/modules/update_product/app/test_update_product_controller.py
+++ tests/modules/update_product/app/test_update_product_controller.py
@@ -1,27 +1,42 @@
 import datetime
+
+import pytest
+
 from src.modules.update_product.app.update_product_controller import UpdateProductController
 from src.modules.update_product.app.update_product_usecase import UpdateProductUsecase
+from src.shared.domain.enums.role_enum import ROLE
+from src.shared.helpers.external_interfaces.http_codes import Forbidden
 from src.shared.helpers.external_interfaces.http_models import HttpRequest
 from src.shared.infra.repositories.product_repository_mock import ProductRepositoryMock
+from src.shared.infra.repositories.user_repository_mock import UserRepositoryMock
+
+repo_user = UserRepositoryMock()
+user_id_with_permission = repo_user.users_list[0].user_id
 
 
 class Test_UpdateProductController:
     def test_update_product_controller(self):
-        repo = ProductRepositoryMock()
-        usecase = UpdateProductUsecase(repo=repo)
+        repo_prod = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo_prod=repo_prod, repo_user=repo_user)
         controller = UpdateProductController(usecase=usecase)
 
         request = HttpRequest(
-            body={
-            'product_id':'8a705b91-c9e9-4353-a755-07f13afafed3',
-            'restaurant':'SOUZA_DE_ABREU',
-            'new_available':True,
-            'new_price':15.0,
-            'new_name':'Nome Atualizado',
-            'new_description':'Descrição Atualizada',
-            'new_prepare_time':20,
-            'new_meal_type':'DRINKS',
-            'new_photo':'new_photo',
+            body={"requester_user": {
+                "sub": repo_user.users_list[0].user_id,
+                "name": repo_user.users_list[0].name,
+                "email": repo_user.users_list[0].email,
+                "custom:isMaua": True
+            },
+                'product_id': '8a705b91-c9e9-4353-a755-07f13afafed3',
+                'restaurant': 'SOUZA_DE_ABREU',
+                'new_available': True,
+                'new_price': 15.0,
+                'new_name': 'Nome Atualizado',
+                'new_description': 'Descrição Atualizada',
+                'new_prepare_time': 20,
+                'new_meal_type': 'DRINKS',
+                'new_photo': 'new_photo',
+
             }
         )
 
@@ -38,21 +53,59 @@ class Test_UpdateProductController:
         assert response.body["product"]["last_update"] == int(datetime.datetime.now().timestamp())
         assert response.body["message"] == "the product was updated"
 
+    def test_update_product_withou_permission(self):
+        repo_prod = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo_prod=repo_prod, repo_user=repo_user)
+        controller = UpdateProductController(usecase=usecase)
+
+        user_withouth_permission = repo_user.users_list[-1]
+        user_withouth_permission.role = ROLE.USER
+
+        request = HttpRequest(
+            body={"requester_user": {
+                "sub": user_withouth_permission.user_id,
+                "name": user_withouth_permission.name,
+                "email": user_withouth_permission.email,
+                "custom:isMaua": True
+            },
+                'product_id': '8a705b91-c9e9-4353-a755-07f13afafed3',
+                'restaurant': 'SOUZA_DE_ABREU',
+                'new_available': True,
+                'new_price': 15.0,
+                'new_name': 'Nome Atualizado',
+                'new_description': 'Descrição Atualizada',
+                'new_prepare_time': 20,
+                'new_meal_type': 'DRINKS',
+                'new_photo': 'new_photo',
+
+            }
+        )
+
+        response = controller(request=request)
+
+        assert response.status_code == 403
+
     def test_update_product_controller_product_id_is_missing(self):
-        repo = ProductRepositoryMock()
-        usecase = UpdateProductUsecase(repo=repo)
+        repo_prod = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo_prod=repo_prod, repo_user=repo_user)
         controller = UpdateProductController(usecase=usecase)
 
         request = HttpRequest(
             body={
-            'restaurant':'SOUZA_DE_ABREU',
-            'new_available':True,
-            'new_price':15.0,
-            'new_name':'Nome Atualizado',
-            'new_description':'Descrição Atualizada',
-            'new_prepare_time':20,
-            'new_meal_type':'DRINKS',
-            'new_photo':'new_photo'
+                'requester_user': {
+                    'sub': repo_user.users_list[0].user_id,
+                    'name': repo_user.users_list[0].name,
+                    'email': repo_user.users_list[0].email,
+                    'custom:isMaua': True
+                },
+                'restaurant': 'SOUZA_DE_ABREU',
+                'new_available': True,
+                'new_price': 15.0,
+                'new_name': 'Nome Atualizado',
+                'new_description': 'Descrição Atualizada',
+                'new_prepare_time': 20,
+                'new_meal_type': 'DRINKS',
+                'new_photo': 'new_photo'
             }
         )
 
@@ -62,20 +115,24 @@ class Test_UpdateProductController:
         assert response.body == "Field product_id is missing"
 
     def test_update_product_controller_restaurant_is_missing(self):
-        repo = ProductRepositoryMock()
-        usecase = UpdateProductUsecase(repo=repo)
+        repo_prod = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo_prod=repo_prod, repo_user=repo_user)
         controller = UpdateProductController(usecase=usecase)
 
         request = HttpRequest(
             body={
-            'product_id':'8a705b91-c9e9-4353-a755-07f13afafed3',
-            'new_available':True,
-            'new_price':15.0,
-            'new_name':'Nome Atualizado',
-            'new_description':'Descrição Atualizada',
-            'new_prepare_time':20,
-            'new_meal_type':'DRINKS',
-            'new_photo':'new_photo'
+                'requester_user': {
+                    'sub': repo_user.users_list[0].user_id,
+                    'name': repo_user.users_list[0].name,
+                                   'email': repo_user.users_list[0].email, 'custom:isMaua': True},
+                'product_id': '8a705b91-c9e9-4353-a755-07f13afafed3',
+                'new_available': True,
+                'new_price': 15.0,
+                'new_name': 'Nome Atualizado',
+                'new_description': 'Descrição Atualizada',
+                'new_prepare_time': 20,
+                'new_meal_type': 'DRINKS',
+                'new_photo': 'new_photo'
             }
         )
 
@@ -85,22 +142,23 @@ class Test_UpdateProductController:
         assert response.body == "Field restaurant is missing"
 
     def test_update_product_controller_restaurant_not_valid(self):
-        repo = ProductRepositoryMock()
-        usecase = UpdateProductUsecase(repo=repo)
+        repo_prod = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo_prod=repo_prod, repo_user=repo_user)
         controller = UpdateProductController(usecase=usecase)
 
         request = HttpRequest(
-            body={
-            'product_id':'8a705b91-c9e9-4353-a755-07f13afafed3',
-            'restaurant':'Souzinha',
-            'new_available':True,
-            'new_price':15.0,
-            'new_name':'Nome Atualizado',
-            'new_description':'Descrição Atualizada',
-            'new_prepare_time':20,
-            'new_meal_type':'DRINKS',
-            'new_photo':'new_photo'
-            }
+            body={'requester_user': {'sub': repo_user.users_list[0].user_id, 'name': repo_user.users_list[0].name,
+                                     'email': repo_user.users_list[0].email, 'custom:isMaua': True},
+                  'product_id': '8a705b91-c9e9-4353-a755-07f13afafed3',
+                  'restaurant': 'Souzinha',
+                  'new_available': True,
+                  'new_price': 15.0,
+                  'new_name': 'Nome Atualizado',
+                  'new_description': 'Descrição Atualizada',
+                  'new_prepare_time': 20,
+                  'new_meal_type': 'DRINKS',
+                  'new_photo': 'new_photo'
+                  }
         )
 
         response = controller(request=request)
@@ -109,21 +167,27 @@ class Test_UpdateProductController:
         assert response.body == "No items found for restaurant"
 
     def test_update_product_controller_meal_type_not_valid(self):
-        repo = ProductRepositoryMock()
-        usecase = UpdateProductUsecase(repo=repo)
+        repo_prod = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo_prod=repo_prod, repo_user=repo_user)
         controller = UpdateProductController(usecase=usecase)
 
         request = HttpRequest(
             body={
-            'product_id':'8a705b91-c9e9-4353-a755-07f13afafed3',
-            'restaurant':'SOUZA_DE_ABREU',
-            'new_available':True,
-            'new_price':15.0,
-            'new_name':'Nome Atualizado',
-            'new_description':'Descrição Atualizada',
-            'new_prepare_time':20,
-            'new_meal_type':'SARGADIN',
-            'new_photo':'new_photo'
+                'requester_user': {
+                    'sub': repo_user.users_list[0].user_id,
+                    'name': repo_user.users_list[0].name,
+                    'email': repo_user.users_list[0].email,
+                    'custom:isMaua': True
+                },
+                'product_id': '8a705b91-c9e9-4353-a755-07f13afafed3',
+                'restaurant': 'SOUZA_DE_ABREU',
+                'new_available': True,
+                'new_price': 15.0,
+                'new_name': 'Nome Atualizado',
+                'new_description': 'Descrição Atualizada',
+                'new_prepare_time': 20,
+                'new_meal_type': 'SARGADIN',
+                'new_photo': 'new_photo'
             }
         )
 
