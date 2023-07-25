d20b3edcab0b8e20dda64a8726f6cf7e94436111
LucasKiller <lucalhardo@gmail.com>
2023-07-25 10:35:19 -0300

    update product role controller

--- src/modules/update_product/app/update_product_controller.py
+++ src/modules/update_product/app/update_product_controller.py
@@ -1,4 +1,5 @@
 from src.shared.helpers.errors.usecase_errors import NoItemsFound
+from src.shared.infra.dto.user_api_gateway_dto import UserApiGatewayDTO
 from .update_product_usecase import UpdateProductUsecase
 from .update_product_viewmodel import UpdateProductViewmodel
 from src.shared.domain.enums.meal_type_enum import MEAL_TYPE
@@ -15,37 +16,37 @@ class UpdateProductController:
 
     def __call__(self, request: IRequest) -> IResponse:
         try:
-            if request.data.get('product_id') is None:
-                raise MissingParameters('product_id')
 
-            if request.data.get('restaurant') is None:
-                raise MissingParameters('restaurant')
+            if request.data.get('requester_user') is None:
+                raise MissingParameters('requester_user')
+
+            requester_user = UserApiGatewayDTO.from_api_gateway(request.data.get('requester_user'))
             
-            restaurants = list()
-            for item in RESTAURANT:
-                restaurants.append(item.value)
+            if request.data.get("product").get("restaurant") is None:
+                raise MissingParameters("restaurant")
 
-            if request.data["restaurant"] not in restaurants:
-                raise NoItemsFound("restaurant")
+            restaurant = request.data.get("product").get('restaurant')
+            if restaurant not in [restaurant_value.value for restaurant_value in RESTAURANT]:
+                raise NoItemsFound('restaurant')
+
+            meal_type = request.data.get("product").get('new_meal_type')
+            if meal_type not in [meal_type_value.value for meal_type_value in MEAL_TYPE]:
+                raise NoItemsFound('new_meal_type')
             
-            meal_types = list()
-            for item in MEAL_TYPE:
-                meal_types.append(item.value)
-
-            if request.data["new_meal_type"] not in meal_types:
-                raise NoItemsFound("new_meal_type")
+            if request.data.get("product").get("product_id") is None:
+                raise MissingParameters("product_id")
 
             product = self.UpdateProductUsecase(
-                product_id=str(request.data.get("product_id")),
-                restaurant=RESTAURANT(request.data.get("restaurant")),
-                new_available=bool(request.data.get("new_available")),
-                new_price=float(request.data.get("new_price")),
-                new_name=str(request.data.get("new_name")),
-                new_description=str(request.data.get("new_description")),
-                new_prepare_time=int(request.data.get("new_prepare_time")),
-                new_meal_type=MEAL_TYPE(request.data.get("new_meal_type")),
-                new_photo=str(request.data.get("new_photo"))
-                )
+                product_id=str(request.data.get("product").get("product_id")),
+                restaurant=RESTAURANT[restaurant],
+                new_available=bool(request.data.get("product").get("new_available")),
+                new_price=float(request.data.get("product").get("new_price")),
+                new_name=str(request.data.get("product").get("new_name")),
+                new_description=str(request.data.get("product").get("new_description")),
+                new_prepare_time=int(request.data.get("product").get("new_prepare_time")),
+                new_meal_type=MEAL_TYPE(request.data.get("product").get("new_meal_type")),
+                new_photo=str(request.data.get("product").get("new_photo")),
+                user_id=str(requester_user.user_id))
             
             viewmodel = UpdateProductViewmodel(product=product)
 
--- tests/modules/update_product/app/test_update_product_controller.py
+++ tests/modules/update_product/app/test_update_product_controller.py
@@ -3,25 +3,35 @@ from src.modules.update_product.app.update_product_controller import UpdateProdu
 from src.modules.update_product.app.update_product_usecase import UpdateProductUsecase
 from src.shared.helpers.external_interfaces.http_models import HttpRequest
 from src.shared.infra.repositories.product_repository_mock import ProductRepositoryMock
+from src.shared.infra.repositories.user_repository_mock import UserRepositoryMock
 
 
 class Test_UpdateProductController:
     def test_update_product_controller(self):
-        repo = ProductRepositoryMock()
-        usecase = UpdateProductUsecase(repo=repo)
+        repo_product = ProductRepositoryMock()
+        repo_user = UserRepositoryMock()
+        usecase = UpdateProductUsecase(repo_product,repo_user)
         controller = UpdateProductController(usecase=usecase)
 
         request = HttpRequest(
             body={
-            'product_id':'8a705b91-c9e9-4353-a755-07f13afafed3',
-            'restaurant':'SOUZA_DE_ABREU',
-            'new_available':True,
-            'new_price':15.0,
-            'new_name':'Nome Atualizado',
-            'new_description':'Descrição Atualizada',
-            'new_prepare_time':20,
-            'new_meal_type':'DRINKS',
-            'new_photo':'new_photo',
+                'requester_user': {
+                    "sub": repo_user.users_list[0].user_id,
+                    "name": repo_user.users_list[0].name,
+                    "email": repo_user.users_list[0].email,
+                    "custom:isMaua": True
+                },
+                'product': {
+                    'product_id':'8a705b91-c9e9-4353-a755-07f13afafed3',
+                    'restaurant':'SOUZA_DE_ABREU',
+                    'new_available':True,
+                    'new_price':15.0,
+                    'new_name':'Nome Atualizado',
+                    'new_description':'Descrição Atualizada',
+                    'new_prepare_time':20,
+                    'new_meal_type':'DRINKS',
+                    'new_photo':'new_photo',
+                }
             }
         )
 
@@ -39,20 +49,30 @@ class Test_UpdateProductController:
         assert response.body["message"] == "the product was updated"
 
     def test_update_product_controller_product_id_is_missing(self):
-        repo = ProductRepositoryMock()
-        usecase = UpdateProductUsecase(repo=repo)
+        repo_product = ProductRepositoryMock()
+        repo_user = UserRepositoryMock()
+        usecase = UpdateProductUsecase(repo_product,repo_user)
         controller = UpdateProductController(usecase=usecase)
 
         request = HttpRequest(
             body={
-            'restaurant':'SOUZA_DE_ABREU',
-            'new_available':True,
-            'new_price':15.0,
-            'new_name':'Nome Atualizado',
-            'new_description':'Descrição Atualizada',
-            'new_prepare_time':20,
-            'new_meal_type':'DRINKS',
-            'new_photo':'new_photo'
+                'requester_user': {
+                    "sub": repo_user.users_list[0].user_id,
+                    "name": repo_user.users_list[0].name,
+                    "email": repo_user.users_list[0].email,
+                    "custom:isMaua": True
+                },
+                'product': {
+
+                    'restaurant':'SOUZA_DE_ABREU',
+                    'new_available':True,
+                    'new_price':15.0,
+                    'new_name':'Nome Atualizado',
+                    'new_description':'Descrição Atualizada',
+                    'new_prepare_time':20,
+                    'new_meal_type':'DRINKS',
+                    'new_photo':'new_photo',
+                }
             }
         )
 
@@ -62,20 +82,30 @@ class Test_UpdateProductController:
         assert response.body == "Field product_id is missing"
 
     def test_update_product_controller_restaurant_is_missing(self):
-        repo = ProductRepositoryMock()
-        usecase = UpdateProductUsecase(repo=repo)
+        repo_product = ProductRepositoryMock()
+        repo_user = UserRepositoryMock()
+        usecase = UpdateProductUsecase(repo_product,repo_user)
         controller = UpdateProductController(usecase=usecase)
 
         request = HttpRequest(
             body={
-            'product_id':'8a705b91-c9e9-4353-a755-07f13afafed3',
-            'new_available':True,
-            'new_price':15.0,
-            'new_name':'Nome Atualizado',
-            'new_description':'Descrição Atualizada',
-            'new_prepare_time':20,
-            'new_meal_type':'DRINKS',
-            'new_photo':'new_photo'
+                'requester_user': {
+                    "sub": repo_user.users_list[0].user_id,
+                    "name": repo_user.users_list[0].name,
+                    "email": repo_user.users_list[0].email,
+                    "custom:isMaua": True
+                },
+                'product': {
+                    'product_id':'8a705b91-c9e9-4353-a755-07f13afafed3',
+
+                    'new_available':True,
+                    'new_price':15.0,
+                    'new_name':'Nome Atualizado',
+                    'new_description':'Descrição Atualizada',
+                    'new_prepare_time':20,
+                    'new_meal_type':'DRINKS',
+                    'new_photo':'new_photo',
+                }
             }
         )
 
@@ -85,21 +115,30 @@ class Test_UpdateProductController:
         assert response.body == "Field restaurant is missing"
 
     def test_update_product_controller_restaurant_not_valid(self):
-        repo = ProductRepositoryMock()
-        usecase = UpdateProductUsecase(repo=repo)
+        repo_product = ProductRepositoryMock()
+        repo_user = UserRepositoryMock()
+        usecase = UpdateProductUsecase(repo_product,repo_user)
         controller = UpdateProductController(usecase=usecase)
 
         request = HttpRequest(
             body={
-            'product_id':'8a705b91-c9e9-4353-a755-07f13afafed3',
-            'restaurant':'Souzinha',
-            'new_available':True,
-            'new_price':15.0,
-            'new_name':'Nome Atualizado',
-            'new_description':'Descrição Atualizada',
-            'new_prepare_time':20,
-            'new_meal_type':'DRINKS',
-            'new_photo':'new_photo'
+                'requester_user': {
+                    "sub": repo_user.users_list[0].user_id,
+                    "name": repo_user.users_list[0].name,
+                    "email": repo_user.users_list[0].email,
+                    "custom:isMaua": True
+                },
+                'product': {
+                    'product_id':'8a705b91-c9e9-4353-a755-07f13afafed3',
+                    'restaurant':'Souzinha_de_abreu',
+                    'new_available':True,
+                    'new_price':15.0,
+                    'new_name':'Nome Atualizado',
+                    'new_description':'Descrição Atualizada',
+                    'new_prepare_time':20,
+                    'new_meal_type':'DRINKS',
+                    'new_photo':'new_photo',
+                }
             }
         )
 
@@ -109,21 +148,30 @@ class Test_UpdateProductController:
         assert response.body == "No items found for restaurant"
 
     def test_update_product_controller_meal_type_not_valid(self):
-        repo = ProductRepositoryMock()
-        usecase = UpdateProductUsecase(repo=repo)
+        repo_product = ProductRepositoryMock()
+        repo_user = UserRepositoryMock()
+        usecase = UpdateProductUsecase(repo_product,repo_user)
         controller = UpdateProductController(usecase=usecase)
 
         request = HttpRequest(
             body={
-            'product_id':'8a705b91-c9e9-4353-a755-07f13afafed3',
-            'restaurant':'SOUZA_DE_ABREU',
-            'new_available':True,
-            'new_price':15.0,
-            'new_name':'Nome Atualizado',
-            'new_description':'Descrição Atualizada',
-            'new_prepare_time':20,
-            'new_meal_type':'SARGADIN',
-            'new_photo':'new_photo'
+                'requester_user': {
+                    "sub": repo_user.users_list[0].user_id,
+                    "name": repo_user.users_list[0].name,
+                    "email": repo_user.users_list[0].email,
+                    "custom:isMaua": True
+                },
+                'product': {
+                    'product_id':'8a705b91-c9e9-4353-a755-07f13afafed3',
+                    'restaurant':'SOUZA_DE_ABREU',
+                    'new_available':True,
+                    'new_price':15.0,
+                    'new_name':'Nome Atualizado',
+                    'new_description':'Descrição Atualizada',
+                    'new_prepare_time':20,
+                    'new_meal_type':'Salgadin',
+                    'new_photo':'new_photo',
+                }
             }
         )
 
