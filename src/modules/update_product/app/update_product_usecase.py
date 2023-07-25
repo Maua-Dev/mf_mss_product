abeb449ee4541cfc9258a630016c953a423f496a
LucasKiller <lucalhardo@gmail.com>
2023-07-25 15:43:53 -0300

    Revert "update product role usecase"
    This reverts commit 3fdfda2629106ebc6b500608a8e8c4494a13de48.

--- src/modules/update_product/app/update_product_usecase.py
+++ src/modules/update_product/app/update_product_usecase.py
@@ -2,24 +2,16 @@ import datetime
 from src.shared.domain.entities.product import Product
 from src.shared.domain.enums.meal_type_enum import MEAL_TYPE
 from src.shared.domain.enums.restaurant_enum import RESTAURANT
-from src.shared.domain.enums.role_enum import ROLE
 from src.shared.domain.repositories.product_repository_interface import IProductRepository
-from src.shared.domain.repositories.user_repository_interface import IUserRepository
 from src.shared.helpers.errors.domain_errors import EntityError
-from src.shared.helpers.errors.usecase_errors import NoItemsFound, UserNotAllowed
+from src.shared.helpers.errors.usecase_errors import NoItemsFound
 
 
 class UpdateProductUsecase:
-    def __init__(self, repo_product: IProductRepository, repo_user: IUserRepository):
-        self.repo_product = repo_product
-        self.repo_user = repo_user
+    def __init__(self, repo: IProductRepository):
+        self.repo = repo
 
-    def __call__(self, product_id: str, restaurant: RESTAURANT, user_id: str, new_available: bool = None, new_price: float = None, new_name: str = None, new_description: str = None, new_prepare_time: int = None, new_meal_type: MEAL_TYPE = None, new_photo: str = None, new_last_update: int = None) -> Product:
-        user = self.repo_user.get_user_by_id(user_id)
-
-        if user.role not in [ROLE.OWNER, ROLE.ADMIN, ROLE.SELLER]:
-            raise UserNotAllowed()
-        
+    def __call__(self, product_id: str, restaurant: RESTAURANT, new_available: bool = None, new_price: float = None, new_name: str = None, new_description: str = None, new_prepare_time: int = None, new_meal_type: MEAL_TYPE = None, new_photo: str = None, new_last_update: int = None) -> Product:
         if type(new_available) != bool:
             raise EntityError("new_available")
         
@@ -47,7 +39,7 @@ class UpdateProductUsecase:
         if not Product.validate_product_id(product_id=product_id):
             raise EntityError("product_id")
         
-        product = self.repo_product.update_product(product_id=product_id, restaurant=restaurant,new_available=new_available, new_price=new_price, new_name=new_name, new_description=new_description, new_prepare_time=new_prepare_time, new_meal_type=new_meal_type, new_photo=new_photo, new_last_update=int(datetime.datetime.now().timestamp()))
+        product = self.repo.update_product(product_id=product_id, restaurant=restaurant,new_available=new_available, new_price=new_price, new_name=new_name, new_description=new_description, new_prepare_time=new_prepare_time, new_meal_type=new_meal_type, new_photo=new_photo, new_last_update=int(datetime.datetime.now().timestamp()))
 
         if product == None:
             raise NoItemsFound("product_id and restaurant")
--- tests/modules/update_product/app/test_update_product_usecase.py
+++ tests/modules/update_product/app/test_update_product_usecase.py
@@ -3,151 +3,100 @@ import pytest
 from src.modules.update_product.app.update_product_usecase import UpdateProductUsecase
 from src.shared.domain.enums.meal_type_enum import MEAL_TYPE
 from src.shared.domain.enums.restaurant_enum import RESTAURANT
-from src.shared.domain.enums.role_enum import ROLE
 from src.shared.helpers.errors.domain_errors import EntityError
 from src.shared.helpers.errors.usecase_errors import NoItemsFound
 from src.shared.infra.repositories.product_repository_mock import ProductRepositoryMock
-from src.shared.infra.repositories.user_repository_mock import UserRepositoryMock
 
 
 class Test_UpdateProductUsecase:
     def test_update_product_usecase(self):
-        repo_prod = ProductRepositoryMock()
-        repo_user = UserRepositoryMock()
-        usecase = UpdateProductUsecase(repo_prod,repo_user)
+        repo = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo=repo)
 
-        user = repo_user.users_list[0]
-        user.role = ROLE.OWNER
+        product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo')
 
-        product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo', user_id=user.user_id)
-
-        assert repo_prod.products[0].available == product.available
-        assert repo_prod.products[0].price == product.price
-        assert repo_prod.products[0].name == product.name
-        assert repo_prod.products[0].description == product.description
-        assert repo_prod.products[0].prepare_time == product.prepare_time
-        assert repo_prod.products[0].meal_type == product.meal_type
-        assert repo_prod.products[0].photo == product.photo
-        assert repo_prod.products[0].last_update == product.last_update
-        assert repo_user.users_list[0].user_id == user.user_id
+        assert repo.products[0].available == product.available
+        assert repo.products[0].price == product.price
+        assert repo.products[0].name == product.name
+        assert repo.products[0].description == product.description
+        assert repo.products[0].prepare_time == product.prepare_time
+        assert repo.products[0].meal_type == product.meal_type
+        assert repo.products[0].photo == product.photo
+        assert repo.products[0].last_update == product.last_update
 
     def test_update_product_product_id_invalid_length(self):
-        repo_prod = ProductRepositoryMock()
-        repo_user = UserRepositoryMock()
-        usecase = UpdateProductUsecase(repo_prod,repo_user)
-
-        user = repo_user.users_list[0]
-        user.role = ROLE.OWNER
+        repo = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo=repo)
 
         with pytest.raises(EntityError):
-            product = usecase(product_id="27cb49f5-4313-49a7-9f84", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo', user_id=user.user_id)
+            product = usecase(product_id="27cb49f5-4313-49a7-9f84", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo')
 
     def test_update_product_not_found_product_id(self):
-        repo_prod = ProductRepositoryMock()
-        repo_user = UserRepositoryMock()
-        usecase = UpdateProductUsecase(repo_prod,repo_user)
-
-        user = repo_user.users_list[0]
-        user.role = ROLE.OWNER
+        repo = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo=repo)
 
         with pytest.raises(NoItemsFound):
-            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afaf666", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo', user_id=user.user_id)
+            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afaf666", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo')
 
     def test_update_product_product_id_invalid_type(self):
-        repo_prod = ProductRepositoryMock()
-        repo_user = UserRepositoryMock()
-        usecase = UpdateProductUsecase(repo_prod,repo_user)
-
-        user = repo_user.users_list[0]
-        user.role = ROLE.OWNER
+        repo = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo=repo)
 
         with pytest.raises(EntityError):
-            product = usecase(product_id=123, restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo', user_id=user.user_id)
+            product = usecase(product_id=123, restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo')
 
     def test_update_product_restaurant_invalid_type(self):
-        repo_prod = ProductRepositoryMock()
-        repo_user = UserRepositoryMock()
-        usecase = UpdateProductUsecase(repo_prod,repo_user)
-
-        user = repo_user.users_list[0]
-        user.role = ROLE.OWNER
+        repo = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo=repo)
 
         with pytest.raises(EntityError):
-            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant="RESTAURANT.SOUZA_DE_ABREU", new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo', user_id=user.user_id)
+            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant="RESTAURANT.SOUZA_DE_ABREU", new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo')
 
     def test_update_product_available_invalid_type(self):
-        repo_prod = ProductRepositoryMock()
-        repo_user = UserRepositoryMock()
-        usecase = UpdateProductUsecase(repo_prod,repo_user)
-
-        user = repo_user.users_list[0]
-        user.role = ROLE.OWNER
+        repo = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo=repo)
 
         with pytest.raises(EntityError):
-            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available='True', new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo', user_id=user.user_id)
+            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available='True', new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo')
 
     def test_update_product_price_invalid_type(self):
-        repo_prod = ProductRepositoryMock()
-        repo_user = UserRepositoryMock()
-        usecase = UpdateProductUsecase(repo_prod,repo_user)
-
-        user = repo_user.users_list[0]
-        user.role = ROLE.OWNER
+        repo = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo=repo)
 
         with pytest.raises(EntityError):
-            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo', user_id=user.user_id)
+            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo')
 
     def test_update_product_name_invalid_type(self):
-        repo_prod = ProductRepositoryMock()
-        repo_user = UserRepositoryMock()
-        usecase = UpdateProductUsecase(repo_prod,repo_user)
-
-        user = repo_user.users_list[0]
-        user.role = ROLE.OWNER
+        repo = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo=repo)
 
         with pytest.raises(EntityError):
-            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name=27.0, new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo', user_id=user.user_id)
+            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name=27.0, new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo')
 
     def test_update_product_description_invalid_type(self):
-        repo_prod = ProductRepositoryMock()
-        repo_user = UserRepositoryMock()
-        usecase = UpdateProductUsecase(repo_prod,repo_user)
-
-        user = repo_user.users_list[0]
-        user.role = ROLE.OWNER
+        repo = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo=repo)
 
         with pytest.raises(EntityError):
-            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description=RESTAURANT.SOUZA_DE_ABREU, new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo', user_id=user.user_id)
+            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description=RESTAURANT.SOUZA_DE_ABREU, new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo')
 
     def test_update_product_prepare_time_invalid_type(self):
-        repo_prod = ProductRepositoryMock()
-        repo_user = UserRepositoryMock()
-        usecase = UpdateProductUsecase(repo_prod,repo_user)
-
-        user = repo_user.users_list[0]
-        user.role = ROLE.OWNER
+        repo = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo=repo)
 
         with pytest.raises(EntityError):
-            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20.2, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo', user_id=user.user_id)
+            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20.2, new_meal_type=MEAL_TYPE.DRINKS, new_photo='new_photo')
 
     def test_update_product_meal_type_invalid_type(self):
-        repo_prod = ProductRepositoryMock()
-        repo_user = UserRepositoryMock()
-        usecase = UpdateProductUsecase(repo_prod,repo_user)
-
-        user = repo_user.users_list[0]
-        user.role = ROLE.OWNER
+        repo = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo=repo)
 
         with pytest.raises(EntityError):
-            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type="MEAL_TYPE.DRINKS", new_photo='new_photo', user_id=user.user_id)
+            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type="MEAL_TYPE.DRINKS", new_photo='new_photo')
 
     def test_update_product_photo_invalid_type(self):
-        repo_prod = ProductRepositoryMock()
-        repo_user = UserRepositoryMock()
-        usecase = UpdateProductUsecase(repo_prod,repo_user)
-
-        user = repo_user.users_list[0]
-        user.role = ROLE.OWNER
+        repo = ProductRepositoryMock()
+        usecase = UpdateProductUsecase(repo=repo)
 
         with pytest.raises(EntityError):
-            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo=1678744638, user_id=user.user_id)
+            product = usecase(product_id="8a705b91-c9e9-4353-a755-07f13afafed3", restaurant=RESTAURANT.SOUZA_DE_ABREU, new_available=True, new_price=15.0, new_name='Nome Atualizado', new_description='Descrição Atualizada', new_prepare_time=20, new_meal_type=MEAL_TYPE.DRINKS, new_photo=1678744638)
