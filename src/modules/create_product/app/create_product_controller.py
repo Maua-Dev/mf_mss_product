4270ee0a7929fe2fcc5f4a5461f4a2ffc3a99ff7
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-08-09 17:39:04 -0300

    Fix bug that raised an error when price was int

--- src/modules/create_product/app/create_product_controller.py
+++ src/modules/create_product/app/create_product_controller.py
@@ -54,13 +54,13 @@ class CreateProductController:
                 raise MissingParameters("prepare_time")
 
             product = self.CreateProductUsecase(available=request.data.get("available"),
-                                                price=request.data.get("price"),
+                                                price=float(request.data.get("price")),
                                                 name=request.data.get("name"),
                                                 description=request.data.get("description"),
                                                 meal_type=MEAL_TYPE[meal_type],
                                                 photo=request.data.get("photo"),
                                                 restaurant=RESTAURANT[restaurant],
-                                                prepare_time=request.data.get("prepare_time"),
+                                                prepare_time=int(request.data.get("prepare_time")),
                                                 user_id=requester_user.user_id)
 
             viewmodel = CreateProductViewmodel(product=product)
