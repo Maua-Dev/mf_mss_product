f609bb3602fd129e987b612f486e762e8d7c7502
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2023-08-10 22:03:08 -0300

    adding photo attribute in user entity

--- src/shared/domain/entities/user.py
+++ src/shared/domain/entities/user.py
@@ -13,8 +13,9 @@ class User(abc.ABC):
     MIN_NAME_LENGTH = 2
     user_id: str
     USER_ID_LENGTH = 36
+    photo: Optional[str] = None
 
-    def __init__(self, name: str, email: str, role: ROLE, user_id: str, restaurant: Optional[RESTAURANT] = None):
+    def __init__(self, name: str, email: str, role: ROLE, user_id: str, restaurant: Optional[RESTAURANT] = None, photo: Optional[str] = None):
         if not User.validate_name(name):
             raise EntityError("name")
         self.name = name
@@ -39,6 +40,11 @@ class User(abc.ABC):
 
         if type(user_id) != str:
             raise EntityError("user_id")  
+        
+        if not self.validate_photo(photo=photo):
+            raise EntityError("photo")
+        self.photo = photo
+        
 
     @staticmethod
     def validate_name(name: str) -> bool:
@@ -66,6 +72,12 @@ class User(abc.ABC):
     def validate_user_id(user_id: str) -> bool:
         if type(user_id) != str: return False
         if len(user_id) != User.USER_ID_LENGTH: return False
+        return True
+    
+    @staticmethod
+    def validate_photo(photo: str) -> bool:
+        if photo is None: return True
+        if type(photo) != str: return False
         return True
 
     def __repr__(self):
