3f4b0b4480ee7f136982b0ecb2622c51661a54d2
RodrigoM2004 <rodrigo.monasterios@outlook.com>
2023-08-14 18:15:31 -0300

    controller with tu

---
+++ src/modules/request_upload_product_photo/app/request_upload_product_photo_controller.py
@@ -0,0 +1,45 @@
+from .request_upload_product_photo_usecase import RequestUploadProductPhotoUsecase
+from src.shared.helpers.errors.domain_errors import EntityError
+from src.shared.helpers.errors.usecase_errors import ForbiddenAction, NoItemsFound
+from src.shared.helpers.errors.controller_errors import MissingParameters, WrongTypeParameter
+from src.shared.helpers.external_interfaces.http_codes import OK, BadRequest, Forbidden, HttpResponse, InternalServerError, NotFound
+from src.shared.helpers.external_interfaces.http_models import HttpRequest
+
+class RequestUploadProductPhotoController:
+    def __init__(self, usecase: RequestUploadProductPhotoUsecase):
+        self.requestUploadSelfieUsecase = usecase
+
+    def __call__(self, request: HttpRequest) -> HttpResponse:
+        try:
+            if request.body.get('product_id') is None:
+                raise MissingParameters('product_id')
+
+            if request.body.get('user_id') is None:
+                raise MissingParameters('user_id')
+
+            presigned_post = self.requestUploadSelfieUsecase(product_id=request.body.get('product_id'), user_id=request.body.get('user_id'))
+
+            message = {"message": f"Foto enviada com sucesso."}
+            response = OK(message) 
+            
+            return response
+
+        except NoItemsFound as err:
+            return NotFound(body=err.message)
+
+        except MissingParameters as err:
+            return BadRequest(body=err.message)
+
+        except ForbiddenAction as err:
+            return Forbidden(body=err.message)
+
+        except WrongTypeParameter as err:
+            return BadRequest(body=err.message)
+
+        except EntityError as err:
+            return BadRequest(body=err.message)
+
+        except Exception as err:
+            return InternalServerError(body=err.args[0])
+        
+        
