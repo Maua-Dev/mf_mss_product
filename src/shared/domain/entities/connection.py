a1dc1849ea607ae3841e553f3561b1c878570431
LucasKiller <lucalhardo@gmail.com>
2023-08-15 21:19:18 -0300

    addding __repr__ function in connection route

--- src/shared/domain/entities/connection.py
+++ src/shared/domain/entities/connection.py
@@ -54,3 +54,6 @@ class Connection(abc.ABC):
         if len(id) != Connection.ID_LENGTH:
             return False
         return True
+    
+    def __repr__(self):
+        return f"Connection(connection_id={self.connection_id}, client_id={self.client_id}, expire_date_seconds={self.expire_date_seconds}, creation_time_seconds={self.creation_time_seconds}, user_id={self.user_id}, restaurant={self.restaurant})"
