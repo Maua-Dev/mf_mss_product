312e8cba4d8237a860c77ba837c6d78d58be1f9f
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-08-29 19:24:17 -0300

    Add update order to order repo mock

--- src/shared/domain/repositories/order_repository_interface.py
+++ src/shared/domain/repositories/order_repository_interface.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 from src.shared.domain.entities.order import Order
+from src.shared.domain.entities.order_product import OrderProduct
 from src.shared.domain.enums.restaurant_enum import RESTAURANT
+from src.shared.domain.enums.status_enum import STATUS
 
 
 class IOrderRepository(ABC):
@@ -10,7 +12,6 @@ class IOrderRepository(ABC):
     @abstractmethod
     def create_order(self, order: Order) -> Order:
         pass
-
 
     @abstractmethod
     def get_order_by_id(self, order_id: str) -> Order:
@@ -21,4 +22,13 @@ class IOrderRepository(ABC):
         """This method return all orders which status are PENDING or PREPARING of a single restaurant  that the user wishes."""
         pass
 
-    
+    @abstractmethod
+    def update_order(self,
+                     order_id: str,
+                     new_products: Optional[List[OrderProduct]] = None,
+                     new_status: Optional[STATUS] = None,
+                     new_total_price: Optional[float] = None,
+                     new_observation: Optional[str] = None,
+                     new_aborted_reason: Optional[str] = None
+                     ):
+        pass
