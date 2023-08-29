0a3d72fc5e3f659c400ae9979a7cedeb912e6e5b
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-08-29 19:39:34 -0300

    Add change order status usecase with UTs

---
+++ src/modules/change_order_status/app/change_order_status_usecase.py
@@ -0,0 +1,28 @@
+from src.shared.domain.entities.order import Order
+from src.shared.domain.enums.role_enum import ROLE
+from src.shared.domain.enums.status_enum import STATUS
+from src.shared.domain.repositories.order_repository_interface import IOrderRepository
+from src.shared.domain.repositories.user_repository_interface import IUserRepository
+from src.shared.helpers.errors.usecase_errors import UserNotAllowed, ForbiddenAction
+
+
+class ChangeOrderStatusUsecase:
+    def __init__(self, repo_order: IOrderRepository, repo_user: IUserRepository):
+        self.repo_order = repo_order
+        self.repo_user = repo_user
+
+    def __call__(self, order_id: str, user_id: str, new_status: STATUS):
+        user = self.repo_user.get_user_by_id(user_id)
+
+        if user.role == ROLE.USER:
+            raise UserNotAllowed()
+
+        if user.role == ROLE.ADMIN:
+            return self.repo_order.update_order(order_id=order_id, new_status=new_status)
+
+        order_to_update: Order = self.repo_order.get_order_by_id(order_id)
+
+        if user.restaurant != order_to_update.restaurant:
+            raise ForbiddenAction("order")
+
+        return self.repo_order.update_order(order_id=order_id, new_status=new_status)
