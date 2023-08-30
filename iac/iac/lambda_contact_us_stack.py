5130cfbf42ff6d9f3f94e4db0fb07f702a3210cb
Felipe Carillo <felipecarillo@outlook.com>
2023-08-29 22:23:33 -0300

    Add function to iac

---
+++ iac/iac/lambda_contact_us_stack.py
@@ -0,0 +1,42 @@
+import os
+
+from aws_cdk import (
+    aws_lambda as lambda_,
+    NestedStack, Duration
+)
+from aws_cdk.aws_apigateway import Resource, CognitoUserPoolsAuthorizer, LambdaIntegration
+from aws_cdk.aws_lambda import LayerVersion
+
+from constructs import Construct
+
+
+class LambdaContactUsStack(Construct):
+    def __init__(self, scope: Construct, api_gateway_resource: Resource, authorizer: CognitoUserPoolsAuthorizer,
+                 lambda_layer: LayerVersion = None, stage: str = None) -> None:
+        super().__init__(scope, "MauaFood_LambdaContactUs")
+
+        module_name = "LambdaContactUs"
+
+        environment_variables = {
+            "FROM_EMAIL": os.environ.get("FROM_EMAIL"),
+            "REPLY_TO_EMAIL": os.environ.get("REPLY_TO_EMAIL"),
+            "HIDDEN_COPY": os.environ.get("HIDDEN_COPY"),
+            "AWS_REGION": os.environ.get("AWS_REGION"),
+            "STAGE": stage
+        }
+
+        function = lambda_.Function(
+            self, module_name,
+            code=lambda_.Code.from_asset(f"../lambda_functions/contact_us"),
+            handler=f"app.send_email_feedback_presenter.lambda_handler",
+            runtime=lambda_.Runtime.PYTHON_3_9,
+            layers=[lambda_layer],
+            memory_size=512,
+            environment=environment_variables,
+            timeout=Duration.seconds(15)
+        )
+
+        api_gateway_resource.add_resource(module_name.replace("_", "-")).add_method("POST",
+                                                                                    integration=LambdaIntegration(
+                                                                                        function),
+                                                                                    authorizer=authorizer)
