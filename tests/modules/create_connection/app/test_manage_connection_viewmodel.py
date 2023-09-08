4d9ec67fe16a088b3585e6019e6ba9c643836862
LucasKiller <lucalhardo@gmail.com>
2023-09-07 21:17:40 -0300

    manage connection viewmodel

---
+++ tests/modules/create_connection/app/test_manage_connection_viewmodel.py
@@ -0,0 +1,25 @@
+from src.modules.create_connection.app.manage_connection_viewmodel import ManageConnectionViewmodel
+from src.shared.domain.entities.connection import Connection
+from src.shared.domain.enums.restaurant_enum import RESTAURANT
+
+
+class Test_CreateConnectionViewmodel:
+    def test_manage_connection_viewmodel(self):
+
+        connection = Connection(connection_id="yu120f88-2c34-45u", api_id="gdc02df8-j2", expire_date_seconds=1682265600, creation_time_seconds=1682262000, user_id="93bc6ada-c0d1-7054-66ab-e17414c48gbf", restaurant=RESTAURANT.HORA_H)
+
+        connection_viewmodel = ManageConnectionViewmodel(connection).to_dict()
+
+        expected = {
+            "connection":{
+                "connection_id": "yu120f88-2c34-45u",
+                "api_id": "gdc02df8-j2",
+                "expire_date_seconds": 1682265600,
+                "creation_time_seconds": 1682262000,
+                "user_id": "93bc6ada-c0d1-7054-66ab-e17414c48gbf",
+                "restaurant": "HORA_H"
+            },
+            "message": "the connection status"
+        }
+
+        assert expected == connection_viewmodel
