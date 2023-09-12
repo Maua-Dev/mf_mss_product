fd98361aac8491309298d38a6f0ccd6b3a5b28ff
RodrigoM2004 <rodrigo.monasterios@outlook.com>
2023-09-12 15:37:12 -0300

    second try

--- iac/iac/iac_stack.py
+++ iac/iac/iac_stack.py
@@ -89,5 +89,5 @@ class IacStack(Stack):
         for f in self.lambda_stack.functions_that_need_dynamo_user_permissions:
             self.dynamo_stack.dynamo_table_user.grant_read_write_data(f)
 
-        self.websocket_stack = WebSocketStack(self, construct_id="MauaFood_WebSocketApi",
-                                              lambda_layer=self.lambda_stack.lambda_layer, environment_variables=ENVIRONMENT_VARIABLES)
+        #self.websocket_stack = WebSocketStack(self, construct_id="MauaFood_WebSocketApi",
+        #                                      lambda_layer=self.lambda_stack.lambda_layer, environment_variables=ENVIRONMENT_VARIABLES)
