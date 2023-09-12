7e432e1a56720840674797c7291cc2925c805418
LucasKiller <lucalhardo@gmail.com>
2023-09-12 20:34:59 -0300

    get all order by user usecase 😛

---
+++ src/modules/get_all_orders_by_user/app/get_all_orders_by_user_usecase.py
@@ -0,0 +1,30 @@
+from typing import List
+from src.shared.domain.entities.order import Order
+from src.shared.domain.enums.role_enum import ROLE
+from src.shared.domain.repositories.order_repository_interface import IOrderRepository
+from src.shared.domain.repositories.user_repository_interface import IUserRepository
+from src.shared.helpers.errors.usecase_errors import NoItemsFound, UnregisteredUser, UserNotAllowed
+
+
+class GetAllOrdersByUserUsecase:
+
+    def __init__(self, repo_order: IOrderRepository, repo_user: IUserRepository):
+        self.repo_order = repo_order
+        self.repo_user = repo_user
+
+    def __call__(self, user_id: str, order_id: str) -> List[Order]:
+        
+        user = self.repo_user.get_user_by_id(user_id)
+
+        if user is None:
+            raise UnregisteredUser()
+
+        if user.role in [ROLE.OWNER, ROLE.ADMIN, ROLE.SELLER]:
+            raise UserNotAllowed()
+        
+        order = self.repo_order.get_order_by_id(order_id)
+
+        if order is None:
+            raise NoItemsFound("order_id")
+
+        return self.repo_order.get_all_orders_by_user(user_id=user_id, order_id=order_id)
---
+++ tests/modules/get_all_orders_by_user/app/test_get_all_orders_by_user_usecase.py
@@ -0,0 +1,50 @@
+import pytest
+from src.modules.get_all_orders_by_user.app.get_all_orders_by_user_usecase import GetAllOrdersByUserUsecase
+from src.shared.helpers.errors.usecase_errors import NoItemsFound, UnregisteredUser, UserNotAllowed
+from src.shared.infra.repositories.order_repository_mock import OrderRepositoryMock
+from src.shared.infra.repositories.user_repository_mock import UserRepositoryMock
+
+
+class Test_GetAllOrdersByUserUsecase:
+    def test_get_all_orders_by_user_usecase(self):
+        repo_order = OrderRepositoryMock()
+        repo_user = UserRepositoryMock()
+        usecase = GetAllOrdersByUserUsecase(repo_order, repo_user)
+
+        user = repo_user.users_list[4]
+        order = repo_order.orders[0]
+
+        get_all_orders = usecase(user_id=user.user_id, order_id=order.order_id)
+
+        assert len(get_all_orders) == 2
+
+    def test_get_all_orders_by_user_user_id_none(self):
+        repo_order = OrderRepositoryMock()
+        repo_user = UserRepositoryMock()
+        usecase = GetAllOrdersByUserUsecase(repo_order, repo_user)
+
+        order = repo_order.orders[0]
+
+        with pytest.raises(UnregisteredUser):
+            get_all_orders = usecase(user_id="Não tem", order_id=order.order_id)
+
+    def test_get_all_orders_by_user_user_not_allowed(self):
+        repo_order = OrderRepositoryMock()
+        repo_user = UserRepositoryMock()
+        usecase = GetAllOrdersByUserUsecase(repo_order, repo_user)
+
+        user = repo_user.users_list[0]
+        order = repo_order.orders[0]
+
+        with pytest.raises(UserNotAllowed):
+            get_all_orders = usecase(user_id=user.user_id, order_id=order.order_id)
+
+    def test_get_all_orders_by_user_order_not_found(self):
+        repo_order = OrderRepositoryMock()
+        repo_user = UserRepositoryMock()
+        usecase = GetAllOrdersByUserUsecase(repo_order, repo_user)
+
+        user = repo_user.users_list[4]
+
+        with pytest.raises(NoItemsFound):
+            get_all_orders = usecase(user_id=user.user_id, order_id="não achou")
