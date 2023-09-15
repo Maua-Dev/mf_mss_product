900adbe2f3742218ca9859b049495fae7e5bd375
LucasKiller <lucalhardo@gmail.com>
2023-09-15 01:01:15 -0300

    get all order by user controller 🥱

---
+++ tests/modules/get_all_orders_by_user/app/test_get_all_orders_by_user_controller.py
@@ -0,0 +1,141 @@
+from src.modules.get_all_orders_by_user.app.get_all_orders_by_user_controller import GetAllOrdersByUserController
+from src.modules.get_all_orders_by_user.app.get_all_orders_by_user_usecase import GetAllOrdersByUserUsecase
+from src.shared.helpers.external_interfaces.http_models import HttpRequest
+from src.shared.infra.repositories.order_repository_mock import OrderRepositoryMock
+from src.shared.infra.repositories.user_repository_mock import UserRepositoryMock
+
+
+class Test_GetAllOrdersByUserController:
+    def test_get_all_orders_by_user_controller(self):
+        repo_order = OrderRepositoryMock()
+        repo_user = UserRepositoryMock()
+        usecase = GetAllOrdersByUserUsecase(repo_order, repo_user)
+        controller = GetAllOrdersByUserController(usecase)
+
+        request = HttpRequest(body={
+            'requester_user': {
+                "sub": repo_user.users_list[5].user_id,
+                "name": repo_user.users_list[5].name,
+                "email": repo_user.users_list[5].email,
+                "custom:isMaua": True},
+            'order_id': repo_order.orders[4].order_id
+        })
+
+        response = controller(request=request)
+        
+        assert response.status_code == 200
+        assert response.data["message"] == "the orders were retrieved"
+
+    def test_get_all_orders_by_user_controller_requester_user_none(self):
+        repo_order = OrderRepositoryMock()
+        repo_user = UserRepositoryMock()
+        usecase = GetAllOrdersByUserUsecase(repo_order, repo_user)
+        controller = GetAllOrdersByUserController(usecase)
+
+        request = HttpRequest(body={
+            'order_id': repo_order.orders[4].order_id
+        })
+
+        response = controller(request=request)
+        
+        assert response.status_code == 400
+        assert response.body == "Field requester_user is missing"
+
+    def test_get_all_orders_by_user_controller_order_id_none(self):
+        repo_order = OrderRepositoryMock()
+        repo_user = UserRepositoryMock()
+        usecase = GetAllOrdersByUserUsecase(repo_order, repo_user)
+        controller = GetAllOrdersByUserController(usecase)
+
+        request = HttpRequest(body={
+            'requester_user': {
+                "sub": repo_user.users_list[5].user_id,
+                "name": repo_user.users_list[5].name,
+                "email": repo_user.users_list[5].email,
+                "custom:isMaua": True},
+        })
+
+        response = controller(request=request)
+        
+        assert response.status_code == 400
+        assert response.body == "Field order_id is missing"
+
+    def test_get_all_orders_by_user_controller_user_not_allowed(self):
+        repo_order = OrderRepositoryMock()
+        repo_user = UserRepositoryMock()
+        usecase = GetAllOrdersByUserUsecase(repo_order, repo_user)
+        controller = GetAllOrdersByUserController(usecase)
+
+        request = HttpRequest(body={
+            'requester_user': {
+                "sub": repo_user.users_list[0].user_id,
+                "name": repo_user.users_list[0].name,
+                "email": repo_user.users_list[0].email,
+                "custom:isMaua": True},
+            'order_id': repo_order.orders[2].order_id
+        })
+
+        response = controller(request=request)
+        
+        assert response.status_code == 403
+        assert response.body == "That type of user has no permission for that action"
+
+    def test_get_all_orders_by_user_controller_unregister_user(self):
+        repo_order = OrderRepositoryMock()
+        repo_user = UserRepositoryMock()
+        usecase = GetAllOrdersByUserUsecase(repo_order, repo_user)
+        controller = GetAllOrdersByUserController(usecase)
+
+        request = HttpRequest(body={
+            'requester_user': {
+                "sub": "sem id",
+                "name": repo_user.users_list[5].name,
+                "email": repo_user.users_list[5].email,
+                "custom:isMaua": True},
+            'order_id': repo_order.orders[4].order_id
+        })
+
+        response = controller(request=request)
+        
+        assert response.status_code == 400
+        assert response.body == "That user is not registered"
+
+    def test_get_all_orders_by_user_controller_order_not_found(self):
+        repo_order = OrderRepositoryMock()
+        repo_user = UserRepositoryMock()
+        usecase = GetAllOrdersByUserUsecase(repo_order, repo_user)
+        controller = GetAllOrdersByUserController(usecase)
+
+        request = HttpRequest(body={
+            'requester_user': {
+                "sub": repo_user.users_list[5].user_id,
+                "name": repo_user.users_list[5].name,
+                "email": repo_user.users_list[5].email,
+                "custom:isMaua": True},
+            'order_id': "4b1e0f88-2c34-3t"
+        })
+
+        response = controller(request=request)
+        
+        assert response.status_code == 404
+        assert response.body == "No items found for order_id"
+
+    def test_get_all_orders_by_user_controller_mismatch_id(self):
+        repo_order = OrderRepositoryMock()
+        repo_user = UserRepositoryMock()
+        usecase = GetAllOrdersByUserUsecase(repo_order, repo_user)
+        controller = GetAllOrdersByUserController(usecase)
+
+        request = HttpRequest(body={
+            'requester_user': {
+                "sub": repo_user.users_list[4].user_id,
+                "name": repo_user.users_list[4].name,
+                "email": repo_user.users_list[4].email,
+                "custom:isMaua": True},
+            'order_id': repo_order.orders[4].order_id
+        })
+
+        response = controller(request=request)
+        
+        assert response.status_code == 400
+        assert response.body == "The user_id does not match with the inserted order_id"
