3dd72a7f200ef5efcbcaaa72f5f8a80f64d1fe8c
João Antônnio Martinez <133376282+JamizM@users.noreply.github.com>
2024-09-11 23:41:20 -0300

    fix(Schedule): Formatting error and others things

--- src/shared/domain/entities/schedule.py
+++ src/shared/domain/entities/schedule.py
@@ -2,7 +2,7 @@ import abc
 from datetime import time
 
 from src.shared.domain.enums.restaurant_enum import RESTAURANT
-from src.shared.helpers.errors.domain_errors import EntityError, EntityParameterExcededMaximumValue 
+from src.shared.helpers.errors.domain_errors import EntityError, EntityParameterTimeError 
 
 class Schedule(abc.ABC): 
     schedule_id: str
@@ -23,26 +23,24 @@ class Schedule(abc.ABC):
             raise EntityError("schedule_id")
         self.schedule_id = schedule_id
     
-        if not isinstance(initial_time or end_time, time):
-            raise EntityError("initial_time") 
-        
-        if end_time <= initial_time:
-            raise EntityParameterExcededMaximumValue("end_time", "initial_time")
-        
+        if not isinstance(initial_time, time):
+            raise EntityError("initial_time")
         self.initial_time = initial_time
+
+        if not isinstance(end_time, time):
+            raise EntityError("end_time")
         self.end_time = end_time
+        
+        if initial_time > end_time:
+            raise EntityParameterTimeError("end_time", "initial_time")
         
         if type(restaurant) != RESTAURANT:
             raise EntityError("restaurant")
         self.restaurant = restaurant
         
         if type(accepted_reservation) != bool:
-            raise EntityError("accepted_reservation") #codigo inacessivel
+            raise EntityError("accepted_reservation")
         self.accepted_reservation = accepted_reservation
-
-    # if type(type(accepted_reservation)) != bool: #! codigo acessivel desta forma
-    #         raise EntityError("accepted_reservation")
-    # self.accepted_reservation = accepted_reservation
 
     def to_dict(self) -> dict:
         return {
@@ -54,9 +52,4 @@ class Schedule(abc.ABC):
         }
     
     def __repr__(self) -> str:
-        return f"scheduled_id: {self.schedule_id}, 
-        initial_time: {self.initial_time}, 
-        end_time: {self.end_time}, 
-        restaurant: {self.restaurant}, 
-        accepted_reservation: {self.accepted_reservation}
-        "
+        return f"scheduled_id: {self.schedule_id}, initial_time: {self.initial_time},  end_time: {self.end_time}, restaurant: {self.restaurant}, accepted_reservation: {self.accepted_reservation}"
