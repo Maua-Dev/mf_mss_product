9dc22945e3b98eddb6aa5cc7a02902ceef3987fb
João Antônnio Martinez <133376282+JamizM@users.noreply.github.com>
2024-09-11 23:40:19 -0300

    feat(EntityParameterError): Specific error handling created to handle entity errors

--- src/shared/helpers/errors/domain_errors.py
+++ src/shared/helpers/errors/domain_errors.py
@@ -22,7 +22,10 @@ class EntityParameterTypeError(EntityError):
     @property
     def message(self):
         return self.__message
-
+    
+class EntityParameterTimeError(BaseError):
+    def __init__(self, initial_time: str, end_time: str):
+        super().__init__(f'Initial time {initial_time} must be less than or equal to end time {end_time}')
 
 class EntityParameterError(EntityError):
     def __init__(self, message: str):
