486c8a1cc3c4b4cb11c7ceafb153aaa69dd5ad37
aivazoglou <gusaivazoglou@gmail.com>
2024-09-30 19:39:27 -0300

    fixing some bugs on Schedule_by_Id

--- src/modules/get_schedule_by_id/app/get_schedule_by_id_presenter.py
+++ src/modules/get_schedule_by_id/app/get_schedule_by_id_presenter.py
@@ -5,14 +5,13 @@ from src.shared.infra.repositories.product_repository_mock import ProductReposit
 from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
 
 #repo_order = Environments.get_order_repo()()
-repo_schedule = Environments.get_order_repo()()
-usecase = GetScheduleByIdUsecase(repo_schedule)
+repo = Environments.get_order_repo()()
+usecase = GetScheduleByIdUsecase(repo)
 controller = GetScheduleByIdController(usecase=usecase)
 
 def lambda_handler(event, context):
 
     httpRequest = LambdaHttpRequest(data=event)
-    httpRequest.data['requester_user'] = event.get('requestContext', {}).get('authorizer', {}).get('claims', None)
     response = controller(httpRequest)
     httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
 
